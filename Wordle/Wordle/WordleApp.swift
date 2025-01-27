6e12617a82c13dd5faabc7e0110f8c56fb3b5eb5
chenchewei <gunattack1120@gmail.com>
2025-01-27 13:41:11 +0800

    add: create new project

---
+++ Wordle/Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Anderson Chen on 2025/1/27.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
